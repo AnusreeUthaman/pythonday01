3be9344522d14db8953dfaa332eb18d678338fd7
AnusreeUthaman <anusreeuthaman02@gmail.com>
2024-09-10 19:57:12 +0530

    Introduction to python ,data types and operators

---
+++ day01.py
@@ -0,0 +1,83 @@
+#INTRODUCTION
+
+#python is a high level interpreted programming language
+#created by Guido Vann Rossum
+#easy to read and write
+#object oriented programming language
+#dynamic typed programming language
+#open source
+#IDLE
+#.py
+
+#Data types
+#string-str-"",''
+#integer-int-1,2,3
+#float-12.3,4.5..
+#boolean-bool-True,False
+#list,tuple,set,dictionary
+
+#Arithematic operators
++
+-
+/
+*
+// floor division-
+** exponentiation
+% modulus-returns reminder
+
+#comparison operators
+>
+<
+>=
+<=
+!=not equal to
+==equal to
+
+
+#Logical operators and or not
+x  y  x and y  x or y  not x  not y
+T  T     T       T       F      F
+T  F     F       T       F      T      
+F  T     F       T       T      F 
+F  F     F       F       T      T
+
+#and operators returns True if both of its operands are True.
+
+#or operators returns True if at least one its operands is True.
+
+#not operators returns the opposite boolean value its operand.
+
+# comments
+single line comment #
+multiline ''' ''',""" """
+
+
+# Data types
+
+print ("hello")
+print ("codeme123##")
+print('vitez')
+print (123)
+# int
+print (5.4)
+# float
+print (True)
+
+print("""hello
+ world""")
+#Doc string
+# Arithematic operators
+print(100+50)
+print(4**2)
+print(100//3)
+print(11%3)
+# comparison operators
+print(5!=5)
+print(5==5)
+print(2<3)
+print(2>1)
+print(1<=2)
+#logical operators
+print(5>10 and 5==5)
+print(3>2 or 3!=3)
+print(not 3>1)
